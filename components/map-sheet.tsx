58c7a0dd273ca8f7aef37766f9865bb02999e4bd
Süleyman Korcan ÜNALAN <103763684+skunalan@users.noreply.github.com>
2025-05-01 17:53:33 +0300

    image picker added

---
+++ components/map-sheet.tsx
@@ -0,0 +1,45 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { BottomSheetModal, BottomSheetModalProvider, BottomSheetView } from '@gorhom/bottom-sheet';
+
+
+const MapSheet = () => {
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+  return (
+    <View>
+      <GestureHandlerRootView style={styles.container}>
+        <BottomSheetModalProvider>
+          <Button onPress={handlePresentModalPress} title="Present Modal" color="black" />
+          <BottomSheetModal ref={bottomSheetModalRef} onChange={handleSheetChanges}>
+            <BottomSheetView style={styles.contentContainer}>
+              <Text>Awesome 🎉</Text>
+            </BottomSheetView>
+          </BottomSheetModal>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
+export default MapSheet;
--- screens/signup.tsx
+++ screens/signup.tsx
@@ -1,92 +1,124 @@
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { Controller, useForm } from 'react-hook-form';
 import { MaskedTextInput } from 'react-native-mask-text';
+import * as ImagePicker from 'expo-image-picker';
+import MapSheet from 'components/map-sheet';
 
 const SignupScreen = () => {
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit, setValue, watch } = useForm({
     defaultValues: {
       name: '',
       surname: '',
       email: '',
       password: '',
       phoneNumber: '',
+      profile_image: '',
     },
   });
+
+  const selectImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsMultipleSelection: false,
+      mediaTypes: ['images'],
+    });
+
+    const uri = result.assets?.[0]?.uri;
+    if (!uri) {
+      return;
+    }
+    setValue('profile_image', uri);
+  };
+
+  const { profile_image } = watch();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <View className="p-6 gap-3">
-      <View className='flex-row gap-2'>
-      <Controller
-        name="name"
-        control={control}
-        render={({ field: { value, onChange } }) => (
-          <TextInput
-            className="h-14 rounded-full border-2 border-gray-300 px-5 flex-1"
-            placeholder="Name"
-            value={value}
-            onChangeText={onChange}
+    <View className="gap-3 p-6">
+      <View className="flex-row gap-3">
+        <TouchableOpacity
+          onPress={selectImage}
+          className="aspect-square w-[100px] flex-row overflow-hidden rounded-full bg-gray-200">
+          <Image className="aspect-square w-[100px]" source={{ uri: profile_image }} />
+        </TouchableOpacity>
+        <View className="flex-1 gap-3">
+          <Controller
+            name="name"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <TextInput
+                className="h-14 flex-1 rounded-full border-2 border-gray-300 px-5"
+                placeholder="Name"
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
           />
-        )}
-      />
-      <Controller
-        name="surname"
-        control={control}
-        render={({ field: { value, onChange } }) => (
-          <TextInput
-            className="h-14 rounded-full border-2 border-gray-300 px-5 flex-1"
-            placeholder="Surname"
-            value={value}
-            onChangeText={onChange}
+          <Controller
+            name="surname"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <TextInput
+                className="h-14 flex-1 rounded-full border-2 border-gray-300 px-5"
+                placeholder="Surname"
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
           />
-        )}
-      />
+        </View>
       </View>
-      <Controller
-        name="email"
-        control={control}
-        render={({ field: { value, onChange } }) => (
-          <TextInput
-            className="h-14 rounded-full border-2 border-gray-300 px-5"
-            placeholder="E-Mail"
-            value={value}
-            onChangeText={onChange}
-          />
-          
-        )}
-      />
-      <Controller
-        name="password"
-        control={control}
-        render={({ field: { value, onChange } }) => (
-          <TextInput
-            className="h-14 rounded-full border-2 border-gray-300 px-5"
-            placeholder="Password"
-            secureTextEntry
-            value={value}
-            onChangeText={onChange}
-          />
-          
-        )}
-      />
-      <Controller
-        name="phoneNumber"
-        control={control}
-        render={({ field: { value, onChange } }) => (
-          <MaskedTextInput
-            style={{
-              height: 51,
-              borderRadius: 9999,
-              borderWidth: 2,
-              borderColor: '#D1D5DB',
-              paddingHorizontal: 20,
-            }}
-            placeholder="Phone"
-            mask={"(999)999 99 99"}
-            value={value}
-            onChangeText={onChange}
-          />
-          
-        )}
-      />
+
+      <View className="mt-3 gap-3">
+        <Controller
+          name="email"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <TextInput
+              className="h-14 rounded-full border-2 border-gray-300 px-5"
+              placeholder="E-Mail"
+              value={value}
+              onChangeText={onChange}
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <TextInput
+              className="h-14 rounded-full border-2 border-gray-300 px-5"
+              placeholder="Password"
+              secureTextEntry
+              value={value}
+              onChangeText={onChange}
+            />
+          )}
+        />
+        <Controller
+          name="phoneNumber"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <MaskedTextInput
+              style={{
+                height: 51,
+                borderRadius: 9999,
+                borderWidth: 2,
+                borderColor: '#D1D5DB',
+                paddingHorizontal: 20,
+              }}
+              placeholder="Phone"
+              mask={'(999)999 99 99'}
+              value={value}
+              onChangeText={onChange}
+            />
+          )}
+        />
+      </View>
+
+      <MapSheet />
 
     </View>
   );
